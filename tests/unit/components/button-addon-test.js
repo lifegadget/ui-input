a7b445ddb34f4a924cb8de831f994eb4071d4b32
Ken Snyder <ken@ken.net>
2015-08-12 13:18:10 +0100

    fix unit tests to include now required boolean operator

--- tests/unit/components/button-addon-test.js
+++ tests/unit/components/button-addon-test.js
@@ -6,6 +6,7 @@ import {
 moduleForComponent('button-addon', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+  unit: true
 });
 
 test('it renders', function(assert) {
--- tests/unit/components/input-addon-test.js
+++ tests/unit/components/input-addon-test.js
@@ -6,6 +6,7 @@ import {
 moduleForComponent('input-addon', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+  unit: true
 });
 
 test('it renders', function(assert) {
