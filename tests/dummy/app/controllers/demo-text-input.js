4adbd04c9895e586ee2200021bc55a55b810ebbe
Ken Snyder <ken@ken.net>
2016-04-06 23:13:42 +0100

    minor deps fix

--- tests/dummy/app/controllers/demo-text-input.js
+++ tests/dummy/app/controllers/demo-text-input.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 const { keys, create } = Object; // jshint ignore:line
-const {computed, observer, $, A, run, on, typeOf, debug, defineProperty, get, set, inject, isEmpty} = Ember;  // jshint ignore:line
+const { RSVP: {Promise, all, race, resolve, defer} } = Ember; // jshint ignore:line
+const { inject: {service} } = Ember; // jshint ignore:line
+const { computed, observe, $, run, on, typeOf } = Ember;  // jshint ignore:line
+const { get, set, debug } = Ember; // jshint ignore:line
+const a = Ember.A; // jshint ignore:line
+
 
 export default Ember.Controller.extend({
-  flashMessages: Ember.inject.service(),
+  flashMessages: service(),
   actions: {
     changed: function(context) {
       const flashMessages = Ember.get(this, 'flashMessages');
