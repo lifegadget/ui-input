3fb9cd6ff0dbfbfd83b3160a04aab8979a317709
Ken Snyder <ken@ken.net>
2016-11-05 11:53:13 +0000

    demo includes mood switching

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,22 +1,11 @@
 import Ember from 'ember';
+import navHandler from 'ui-navigation/mixins/nav-handler';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(navHandler, {
 
   isRepressed: false,
   toggledEnablement: false,
   isIndexPage: Ember.computed.equal('currentPath', 'index'),
   notIndexPage: Ember.computed.not('isIndexPage'),
-
-  actions: {
-    navChange(context) {
-      const {routeTo, linkTo} = context[0];
-      if(linkTo) {
-        window.open(linkTo);
-      }
-      if (routeTo) {
-        this.transitionToRoute(routeTo);
-      }
-    }
-  }
 
 });
