c95e8b9841ead5315d55cf08ba21c4a3249c1f57
Ken Snyder <ken@ken.net>
2016-11-06 20:13:54 +0000

    further UT improvement

--- tests/integration/components/email-input-test.js
+++ tests/integration/components/email-input-test.js
@@ -1,7 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import Ember from 'ember';
-
 
 moduleForComponent('email-input', 'Integration | Component | email input', {
   integration: true
@@ -26,56 +24,29 @@ test('it renders', function(assert) {
 });
 
 test('validity check on empty @ instantiation', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
+  assert.expect(1);
   this.set('value', null);
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  assert.equal(this.get('validity'), 'empty', 'null value is set to empty validity');
-
+  this.on('vTest', function(result) {
+    assert.equal(result.validity, 'empty', 'null value is set to "empty" validity');
+  });
+  this.render(hbs`{{email-input value=value onValidity=(action 'vTest')}}`);
 });
 
 test('validity check on valid @ instantiation', function(assert) {
-
+  assert.expect(1);
   this.set('value', 'jack@johnson.com');
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  assert.equal(this.get('validity'), 'valid', 'valid email tests positively');
-
+  this.on('vTest', function(result) {
+    assert.equal(result.validity, 'valid', 'valid email tests positively');
+  });
+  this.render(hbs`{{email-input value=value onValidity=(action 'vTest')}}`);
 });
 
 test('validity check on invalid @ instantiation', function(assert) {
-
+  assert.expect(1);
   this.set('value', 'jack@johnson');
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  assert.equal(this.get('validity'), 'invalid', 'invalid email tests negatively');
-
-});
-
-
-test('validity check through empty @ lifecycle', function(assert) {
-
-  this.set('value', null);
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  assert.equal(this.get('validity'), 'empty', 'null value is set to empty validity');
-
-});
-
-test('validity check through valid @ lifecycle', function(assert) {
-
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  Ember.run.next(() => {
-    this.$('input').val('jack@johnson.com');
-    this.$('input').trigger('input');
-    assert.equal(this.get('validity'), 'valid', 'valid email tests positively');
+  this.on('vTest', function(result) {
+    assert.equal(result.validity, 'invalid', 'invalid email tests negatively');
   });
-
+  this.render(hbs`{{email-input value=value onValidity=(action 'vTest')}}`);
 });
 
-test('validity check through invalid @ lifecycle', function(assert) {
-
-  this.render(hbs`{{email-input value=value onValidity=(mut validity)}}`);
-  Ember.run.next(() => {
-    this.$('input').val('jack').trigger('input');
-    assert.equal(this.get('validity'), 'invalid', 'invalid email tests negatively');
-  });
-
-});
